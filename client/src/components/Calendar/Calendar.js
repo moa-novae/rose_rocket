a791618c3a66dd7702c2f8ee9c3566bf7f1c1504
Pumpkin-spice-cloud <alan_mo@live.com>
2020-07-26 11:24:36 -0400

    Style: week changer arrows are fixed space aprt

--- client/src/components/Calendar/Calendar.js
+++ client/src/components/Calendar/Calendar.js
@@ -11,7 +11,7 @@ export default function () {
   const days = ["Sun", "Mon", "Tue", "Thu", "Fri", "Sat"];
 
   //task of each day is passed to each DayColumn
-  console.log('week', weeklyTasks)
+  console.log("week", weeklyTasks);
   const dayColumns = days.map((day, i) => {
     const dailyTasks = weeklyTasks.filter((task) => task.time.day === i);
     return <DayColumn day={day} dailyTasks={dailyTasks} />;
@@ -20,17 +20,19 @@ export default function () {
     <div className="calendar">
       <div className="calendar-header">
         <span className="calendar-week">{`Week ${week}`}</span>
-        <span className="calendar-week-btn">
-          <FontAwesomeIcon
-            icon={faArrowLeft}
-            onClick={() => changeWeekBy(-1)}
-          />
-        </span>
-        <span className="calendar-week-btn">
-          <FontAwesomeIcon
-            icon={faArrowRight}
-            onClick={() => changeWeekBy(1)}
-          />
+        <span className="calendar-week-btns">
+          <span className="calendar-week-btn">
+            <FontAwesomeIcon
+              icon={faArrowLeft}
+              onClick={() => changeWeekBy(-1)}
+            />
+          </span>
+          <span className="calendar-week-btn">
+            <FontAwesomeIcon
+              icon={faArrowRight}
+              onClick={() => changeWeekBy(1)}
+            />
+          </span>
         </span>
       </div>
       <div className="calendar-body">
