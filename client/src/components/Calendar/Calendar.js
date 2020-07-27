29b9ca75b54f25a9ecbd2221b0c248189114e459
Pumpkin-spice-cloud <alan_mo@live.com>
2020-07-27 15:24:19 -0400

    Refactor: calendar header to its own component

--- client/src/components/Calendar/Calendar.js
+++ client/src/components/Calendar/Calendar.js
@@ -1,15 +1,10 @@
 import React from "react";
 import DayColumn from "./DayColumn/DayColumn";
 import TimeColumn from "./TimeColumn/TimeColumn";
+import CalendarHeader from "./CalendarHeader/CalendarHeader";
 import useCalendar from "./useCalendar";
 import "./calendar.scss";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faChevronLeft,
-  faChevronRight,
-  faArrowRight,
-} from "@fortawesome/free-solid-svg-icons";
 export default function () {
   const {
     changeWeekBy,
@@ -27,35 +22,13 @@ export default function () {
   });
   return (
     <div className="calendar">
-      <div className="calendar-header">
-        <div className="calendar-header-left">
-          <div className="calendar-week">{`Week ${week}`}</div>
-          <div className="calendar-week-btns">
-            <div className="calendar-week-btn">
-              <FontAwesomeIcon
-                icon={faChevronLeft}
-                onClick={() => changeWeekBy(-1)}
-              />
-            </div>
-            <div className="calendar-week-btn">
-              <FontAwesomeIcon
-                icon={faChevronRight}
-                onClick={() => changeWeekBy(1)}
-              />
-            </div>
-          </div>
-        </div>
-        <div className="calendar-header-right">
-          Change week to
-          <input
-            // not using type='number'
-            // since easier to use regex to limit value to 0<=x<-51
-            value={weekInput}
-            onChange={handleOnChange}
-          />
-          <FontAwesomeIcon icon={faArrowRight} onClick={handleWeekJump} />
-        </div>
-      </div>
+      <CalendarHeader
+        week={week}
+        changeWeekBy={changeWeekBy}
+        weekInput={weekInput}
+        handleOnChange={handleOnChange}
+        handleWeekJump={handleWeekJump}
+      />
       <div className="calendar-body">
         <TimeColumn />
         {dayColumns}
---
+++ client/src/components/Calendar/CalendarHeader/CalendarHeader.js
@@ -0,0 +1,47 @@
+import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+  faArrowRight,
+} from "@fortawesome/free-solid-svg-icons";
+export default function CalendarHeader(props) {
+  const {
+    week,
+    changeWeekBy,
+    weekInput,
+    handleOnChange,
+    handleWeekJump,
+  } = props;
+  return (
+    <div className="calendar-header">
+      <div className="calendar-header-left">
+        <div className="calendar-week">{`Week ${week}`}</div>
+        <div className="calendar-week-btns">
+          <div className="calendar-week-btn">
+            <FontAwesomeIcon
+              icon={faChevronLeft}
+              onClick={() => changeWeekBy(-1)}
+            />
+          </div>
+          <div className="calendar-week-btn">
+            <FontAwesomeIcon
+              icon={faChevronRight}
+              onClick={() => changeWeekBy(1)}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="calendar-header-right">
+        Change week to
+        <input
+          // not using type='number'
+          // since easier to use regex to limit value to 0<=x<-51
+          value={weekInput}
+          onChange={handleOnChange}
+        />
+        <FontAwesomeIcon icon={faArrowRight} onClick={handleWeekJump} />
+      </div>
+    </div>
+  );
+}
