52d0573bc0465cf2e9295813b96ff99273f9de53
Pumpkin-spice-cloud <alan_mo@live.com>
2020-07-30 19:09:49 -0400

    Remove files not used

--- index.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-const app = express();
-const port = process.env.PORT || 5000;
-
-// console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
-
